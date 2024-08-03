f90f28a660764f931bd909f798f7f986fc699eed
romanzc2011 <romanzc2011@gmail.com>
2024-08-03 17:28:53 -0500

    Assignment-3-2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -10,8 +10,11 @@ KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.gi
 KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
+ASSIGN2_FINDER_WRITER=/home/romancampbell/Coursera/Linux_Intro_to_Buildroot/assignment-2-romanzc2011/finder-app
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+SYSROOT=$(which ${CROSS_COMPILE}gcc)
+SYSROOT_LIB=$(dirname $SYSROOT)/../aarch64-none-linux-gnu/libc
 
 if [ $# -lt 1 ]
 then
@@ -29,17 +32,33 @@ if [ ! -d "${OUTDIR}/linux-stable" ]; then
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "make mrproper============================================="
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    echo "make defconfig============================================"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    echo "make all=================================================="
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    echo "SKIPPING make modules=============================================="
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+
+    echo "make dtbs================================================="
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp -rp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
-echo "Creating the staging directory for the root filesystem"
+
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
@@ -47,34 +66,80 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Creating rootfs staging directory
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
 
-cd "$OUTDIR"
+
+# TODO: Create necessary base directories
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin 
+mkdir -p var/log
+
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+echo "Installing busybox.................................."
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+echo "busybox install SUCCESS............................."
 
-echo "Library dependencies"
+# Setting proper permissions for busybox
+sudo chmod 4755 ${OUTDIR}/rootfs/bin/busybox
+
+cd ${OUTDIR}/rootfs
+
+echo "Library dependencies................................"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp ${SYSROOT_LIB}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib 
 
-# TODO: Make device nodes
+cp ${SYSROOT_LIB}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT_LIB}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64 
+cp ${SYSROOT_LIB}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+echo "Library copy complete..............................."
 
-# TODO: Clean and build the writer utility
+echo "Make device nodes..................................."
+if [ ! -e ${OUTDIR}/rootfs/dev/null ];
+then
+    sudo mknod -m 666 dev/null c 1 3 
+elif [ ! -e ${OUTDIR}/dev/console ];
+then
+    sudo mknod -m 600 dev/console c 5 1
+else
+    echo "null and console devices already exist"
+fi
+echo "Device nodes SUCCESS................................"
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "Copying finder/writer programs ....................."
+cp ${ASSIGN2_FINDER_WRITER}/Makefile ${OUTDIR}/rootfs/home
+cp ${ASSIGN2_FINDER_WRITER}/finder-test.sh ${OUTDIR}/rootfs/home
+cp ${ASSIGN2_FINDER_WRITER}/finder.sh ${OUTDIR}/rootfs/home 
+cp ${ASSIGN2_FINDER_WRITER}/writer.c ${OUTDIR}/rootfs/home
 
-# TODO: Chown the root directory
+cp -r ${ASSIGN2_FINDER_WRITER}/../conf ${OUTDIR}/rootfs/home
+cd ${OUTDIR}/rootfs/home
+make CROSS_COMPILE=aarch64-none-linux-gnu-
 
-# TODO: Create initramfs.cpio.gz
+cp ${ASSIGN2_FINDER_WRITER}/../conf/* ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home 
+
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root ${OUTDIR}/rootfs
+
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip ${OUTDIR}/initramfs.cpio
+echo "Build COMPLETE"
